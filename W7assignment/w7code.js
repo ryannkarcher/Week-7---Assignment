171ab8286d3683417855a752f3d5c112efc613fe
Ryann Crossett <ryanncrossett@Ryanns-MacBook-Pro.local>
2023-11-12 16:10:05 -0700

    I think I finished the project

--- W7assignment/w7code.js
+++ W7assignment/w7code.js
@@ -16,19 +16,19 @@ console.log("First element was subtracted from the last element, resulting in", 
       //same formula as earlier; new element is last and changes the response.
 console.log("First element was subtracted from the new last element, resulting in", subFirstFromLast); //24-3=21
    function findAverage(array) {
-      let sum=0;
-         //sum starts at 0 since we are calculating it
+      let sum1=0;
+         //sum1 starts at 0 since we are calculating it
       for (let i=0; i<array.length; i++) {
          //for loop to count numbers up to array length
-         sum += array[i];
+         sum1 += array[i];
          //adds the array numbers together
       }
-         let average= sum/array.length;
+         let average= sum1/array.length;
             //calculates array average
          return average;
             //returns array average
    }
-console.log("The average of the ages in this array was", findAverage(agesArray)); //sum=254/length=9 = 28.22...
+console.log("The average of the ages in this array was", findAverage(agesArray)); //sum1=254/length=9 = 28.22...
 
 
 
@@ -92,4 +92,114 @@ console.log("Question 5:");
             //return the length of the elements, so the number of letters
             // in the names
       });
-      console.log("This is the new array of name lengths:",nameLengths);
+      console.log("This is the new array of name lengths:", nameLengths);
+
+
+//QUESTION 6
+      //reqs: 1) loop to claculate sum of nameLengths
+console.log("Question 6:");
+   let sum2=0;
+         //sum2 starts at 0 since we are calculating it
+      for (let i=0; i<nameLengths.length; i++) {
+         //for loop to count numbers up to array length
+         sum2 += nameLengths[i];
+         //adds the array numbers together
+      }
+console.log("This is the sum of nameLengths:", sum2, "which you'll see is the same as the number of characters in the array of Question 1");
+
+
+//QUESTION 7
+      //reqs: 1) function w/ 2 param (word and n) and returns word concatenated
+console.log("Question 7:");
+      function repeatString(word,n) {
+         // function name has arguments for inputs word and n (number of times to repeat)
+         let result = '';
+            //since it is a string, instead of =0, it is = ''
+         for (let i=0; i<n; i++) {
+            //repeat up to the number of times given
+            result +=word;
+               //result has addition assignment that it increases and assigns itself to a new value
+               // e.g. wordword.
+         }
+         return result;
+      }
+   console.log(repeatString('Booyah!',3));
+      //returns Booyah! less than 3 times, but it includes 0
+      // so it's still 3 times (0,1,2)
+
+
+//QUESTION 8
+      //reqs: 1) function two params, returns full name with a space
+console.log("Question 8:");
+      function createFullName(firstName, lastName) {
+         console.log(firstName+" "+lastName);
+      }
+createFullName("Miranda", "Emory");
+      //calls on function
+
+
+//QUESTION 9
+      //reqs: 1) function takes array of numbers and returns true if
+      // sum is >100
+console.log("Question 9:");
+   function sumGreaterThan100 (array) {
+      let sum3=0;
+         // sum3 starts at 0 since we are calculating it
+      for (let i=0; i<array.length; i++) {
+         //for loop to count numbers up to array length
+      sum3 += array[i];
+         //adds the array numbers together
+      }
+         if (sum3>100) {
+            return true;
+         } else {
+            return false;
+         }
+   }
+let exampleArray = [1,2,3,4,5,6,7,1,9,3,5,3,4,7,5];
+   // example that I can change values to change my answers
+console.log("This array's sum being larger than 100 is:", sumGreaterThan100(exampleArray));
+
+
+//QUESTION 10
+   //reqs: 1)function takes array of num and retuns average
+console.log("Question 10:");
+   console.log("The average of the ages in this array was", findAverage(exampleArray));
+      //called from findAverage function from Q1. used exampleArray
+   
+//QUESTION 11
+   //reqs: 1) function 2 arrays of numbers and returns true if average
+   // of first array is greater than average of second
+      function greaterAverage (array1, array2) {
+         let avg1= findAverage(array1);
+         let avg2= findAverage(array2);
+            if (avg1>avg2) {
+               return true;
+            } else {
+               return false;
+            }
+      }
+   let array1 = [30,40];
+   let array2 = [50,1];
+console.log("array1 is greater than array2:", greaterAverage(array1, array2));
+
+
+//QUESTION 12
+      //reqs: 1) function with boolean and number
+console.log("Question 12:");
+      function willBuyDrink (temp, money) {
+         if (temp===true) {
+            if (money>10.50){
+               return true;
+            } else {
+               return false;
+                  //need this line so the temp=false, money>10.5 is correct
+            }
+         } else {
+            return false;
+               //all other options, are false
+         }
+      }
+      let isHotOutisde = true;
+      let moneyInPocket = 13;
+   console.log("I will buy a drink", willBuyDrink(isHotOutisde, moneyInPocket));
